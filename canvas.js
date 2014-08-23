645c46fa0456b6d18bb7579dd85b2ba2a9a89070
Shreyas <skraman1999@gmail.com>
2014-08-23 15:27:49 -0700

    Draw on click.

--- canvas.js
+++ canvas.js
@@ -1,3 +1,26 @@
-var g = document.getElementById('canvas').getContext('2d');
+canvas = document.getElementById('canvas')
+canvas.addEventListener('click', onClick, false);
+var g = canvas.getContext('2d');
 g.fillStyle='#FF00A5';
-g.fillRect(0,0,100,100);
+
+function onClick(e) {
+  var pp = getPosition(e.currentTarget);
+  var xp = e.clientX - pp.x;
+  var yp = e.clientY - pp.y;
+
+  console.log(xp + " " + yp);
+
+  g.fillRect(xp,yp,10,10);
+}
+
+function getPosition(e) {
+    var xp = 0;
+    var yp = 0;
+    
+    while (e) {
+        xp += (e.offsetLeft - e.scrollLeft + e.clientLeft);
+        yp += (e.offsetTop - e.scrollTop + e.clientTop);
+        e = e.offsetParent;
+    }
+    return { x: xp, y: yp };
+}
