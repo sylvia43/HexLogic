c2e42c5e766243927f4c15ca7e29c16222e7239a
Shreyas <skraman1999@gmail.com>
2014-08-23 22:13:18 -0700

    Automatically draw, bigger grid.

--- canvas.js
+++ canvas.js
@@ -7,12 +7,18 @@ var map = [
   [{state:0, type:0}, {state:0, type:0}, {state:0, type:0}, {state:0, type:0}, {state:0, type:0}, {state:0, type:0}],
 ];
 
-var size = 10;
+var size = 30;
 
 canvas = document.getElementById('canvas')
 canvas.addEventListener('click', onClick, false);
 var g = canvas.getContext('2d');
 g.fillStyle='#FF00A5';
+
+for (var i=0; i<map.length; i++) {
+  for (var j=0; j<map[i].length; j++) {
+    drawHex(map[i][j], i*size*3/2, (i+j*2)*size*Math.sqrt(3)/2);
+  }
+}
 
 function onClick(e) {
   var pp = getPosition(e.currentTarget);
@@ -20,12 +26,6 @@ function onClick(e) {
   var yp = e.clientY - pp.y;
 
   console.log(xp + " " + yp);
-
-  for (var i=0; i<map.length; i++) {
-    for (var j=0; j<map[i].length; j++) {
-      drawHex(map[i][j], i*size*3/2, (i+j*2)*size*Math.sqrt(3)/2);
-    }
-  }
 }
 
 function drawHex(hex, xp, yp) {
